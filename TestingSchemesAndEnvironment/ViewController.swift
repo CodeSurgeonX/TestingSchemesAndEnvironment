053108a3def4be458636e260b2afa8e74a95ed98
Shashwat Kashyap <shashwat.kashyap@icloud.com>
2024-02-26 16:31:22 +0530

    Setup

--- TestingSchemesAndEnvironment/ViewController.swift
+++ TestingSchemesAndEnvironment/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let vc = UIHostingController(rootView: ExampleView())
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true)
+    }
 }
 
+struct ExampleView: View {
+    var body: some View {
+        VStack {
+            Text("Hello world!")
+                .font(.largeTitle)
+                .foregroundColor(.green)
+            
+            Circle()
+                .foregroundColor(.orange)
+                .frame(minHeight: 300)
+                .border(.blue)
+                .overlay {
+                    GeometryReader { proxy in
+                        Text("I will always stay inside the circle I will always stay inside the circle I will always stay inside the circle I will always stay inside the circle I will always stay inside the circle")
+                            .font(.largeTitle)
+                            .frame(maxWidth: proxy.size.width, maxHeight: proxy.size.height)
+                    }
+                }
+        }
+    }
+}
